83fd9e3a1cb9512e3e0dc8ac6aad3fcc269eba53
msrajan0501 <91381254+msrajan0501@users.noreply.github.com>
2022-04-04 20:25:00 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "I'm learning java";
+		for (int i = s.length() - 1; i >= 0; i--)
+
+		{
+			System.out.print(s.charAt(i));
+		}
+
+	}
+
+}
